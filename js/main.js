5181198a303577cf08aa246d641e46e7556f0194
Taryn <taryn@sugarpush.com.au>
2018-02-20 12:50:41 +1100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped Queen");
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!") }
+	else {
+	alert("Sorry, try again.")
+	};
+}
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped King");
+var flipCard = function (cardID) {
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!") }
-else {
-alert("Sorry, try again.")
-};
+console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard (0);
+flipCard (2);
